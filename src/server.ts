6e2a36da91ad2b07e564e727a39fc9159a88b03e
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-09 22:54:31 -0500

    add connection to Postgresql in TablePlus

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import express from 'express'
 import router from './router';
+import db from './config/db';
+
+
+//Conectar a db
+async function connectDB() {
+
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('conexion a db exitosa')
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+connectDB()
 
 const server = express()
-
 
 server.use('/', router)
 
