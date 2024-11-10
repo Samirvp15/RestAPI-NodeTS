6c843cdb6b6866f6e8cd14b69b10b9f4e21acd04
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-10 12:35:21 -0500

    add PUT, PATCH and DELETE methods in ProductRouter

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import Product from '../models/Product.model'
+import { body } from 'express-validator'
 
 
 
@@ -34,10 +35,6 @@ export const getProductById = async (req: Request, res: Response): Promise<any> 
 
 }
 
-
-
-
-
 export const createProduct = async (req: Request, res: Response) => {
 
     try {
@@ -48,3 +45,64 @@ export const createProduct = async (req: Request, res: Response) => {
     }
 
 }
+
+export const updateProduct = async (req: Request, res: Response): Promise<any> => {
+
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+        return res.status(404).json({
+            error: 'Producto no encontrado'
+        })
+    }
+
+    //Actualizar
+    await product.update(req.body)
+    await product.save()
+
+
+    res.json({ data: product })
+
+}
+
+export const updateAvailability = async (req: Request, res: Response): Promise<any> => {
+
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+        return res.status(404).json({
+            error: 'Producto no encontrado'
+        })
+    }
+
+    //Actualizar
+    product.availability = req.body.availability
+    await product.save()
+
+
+    res.json({ data: product })
+
+}
+
+
+export const deleteProduct = async (req: Request, res: Response): Promise<any> => {
+
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+        return res.status(404).json({
+            error: 'Producto no encontrado'
+        })
+    }
+
+    await product.destroy()
+
+
+    res.json({ data: 'Producto Eliminado' })
+
+}
+
+
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 
 import { Router } from 'express'
-import { createProduct, getProducts, getProductById } from './handlers/product'
+import { createProduct, getProducts, getProductById, updateProduct, updateAvailability, deleteProduct } from './handlers/product'
 import { body, param } from 'express-validator'
 import { handleInputErrors } from './middlewares'
 
@@ -8,13 +8,13 @@ import { handleInputErrors } from './middlewares'
 const router = Router()
 
 router.get('/', getProducts)
+
 router.get('/:id',
     param('id').isInt().withMessage('ID no valido'),
     handleInputErrors,
     getProductById)
 
 router.post('/',
-
     //Validacion
     body('price').
         isNumeric().withMessage('Valor no valido').
@@ -28,17 +28,30 @@ router.post('/',
     handleInputErrors,
     createProduct)
 
-router.put('/', (req, res) => {
-    res.json('Hola PUT')
-})
+router.put('/:id',
+    //Validacion
+    body('price').
+        isNumeric().withMessage('Valor no valido').
+        isFloat().withMessage('Valor no flotante').
+        notEmpty().withMessage('Precio no puede ser vacio').
+        custom(value => value > 0).withMessage('Precio no valido'),
+    body('name').
+        notEmpty().withMessage('Nombre no puede ser vacio'),
+    body('availability').
+        isBoolean().withMessage('Valor no valido'),
+    //Middleware
+    handleInputErrors,
+    updateProduct)
 
-router.patch('/', (req, res) => {
-    res.json('Hola PATCH')
-})
+router.patch('/:id',
+    param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    updateAvailability)
 
-router.delete('/', (req, res) => {
-    res.json('Hola DELETE')
-})
+router.delete('/:id', param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    deleteProduct
+)
 
 
 export default router
