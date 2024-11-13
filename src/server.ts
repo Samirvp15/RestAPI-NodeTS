4ff712eab1b5660dd174a0cc2fbdac8ab18ef1e2
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-12 21:40:12 -0500

    fix AllowedURLCORS for our Backend and minor bug

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import router from './router';
-import swaggerUi  from 'swagger-ui-express';
+import swaggerUi from 'swagger-ui-express';
 import morgan from 'morgan'
 import db from './config/db';
 import swaggerSpec from './config/swagger';
@@ -24,14 +24,14 @@ connectDB()
 const server = express()
 server.use(express.json())
 
-
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.BACKEND_UR];
 const corsOptions: CorsOptions = {
-    origin: function(origin, callback){
-       if(origin === process.env.FRONTEND_URL){
-        callback(null, true)
-       }else{
-        callback(new Error('Error de CORS'))
-       }
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS'))
+        }
     }
 }
 
